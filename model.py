97db83c844065ff92900b03fc8ab110adee3c000
Ghass <gam20@mail.aub.edu>
2021-05-09 14:31:20 +0200

    Added model compile method, requirements and jupyter notebook

--- model.py
+++ model.py
@@ -1,14 +1,24 @@
 import importlib
 
 import tensorflow as tf
-from tensorflow import keras
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import BatchNormalization, GlobalAveragePooling2D
-from tensorflow.keras.layers import Dense, Lambda, Dropout, Activation, Flatten, Input
+from tensorflow.keras.layers import Dense, Dropout, Activation, Input
 
-class MultiOutputModel():
+class MultiOutputModel(object):
     """
     """
+    def __init__(self, width, height, channels, outputs):
+        """
+        """
+        self.width = width
+        self.width = height
+        self.outputs = outputs
+        self.model = None
+        self.backbone = None
+        self.optimizer = None
+        self.input_tensor = Input(shape=(width, height, channels))
+
     def _generic_layer(self, inputs):
         """
         """
@@ -21,51 +31,59 @@ class MultiOutputModel():
         
         return x 
    
-    def _generic_branch(self, base_model, structure):
+    def _generic_branch(self, base_model, output):
         """
         """
         # x = Lambda(lambda c: tf.image.rgb_to_grayscale(c))(inputs)        
         x = self._generic_layer(base_model)
-        x = Dense(structure[1])(x)
-        x = Activation(structure[2], name=structure[0])(x)       
+        x = Dense(output['outNeurons'])(x)
+        x = Activation(output['outActivation'], name=output['type'])(x)       
         
         return x
     
-    def _base_model(self, base, input_tensor):
+    def _base_model(self, backbone, input_tensor):
         """
         """
-        feature_extractor = importlib.import_module("tensorflow.keras.applications").__getattribute__(base)
+        feature_extractor = importlib.import_module("tensorflow.keras.applications").__getattribute__(backbone)
 
         base_model = feature_extractor(input_tensor=input_tensor, weights='imagenet', include_top=False)
         base_model.trainable = False
 
         return base_model
     
-    def build_model(self, width, height, base, structures):
+    def build_model(self, backbone):
         """
         """
-        input_tensor = Input(shape=(width, height, 3))
-        
-        base_model = self._base_model(base, input_tensor)
+        self.backbone = backbone
+        base_model = self._base_model(self.backbone, self.input_tensor)
         
         x = base_model.output
         x = GlobalAveragePooling2D()(x)
 
         branches = []
 
-        for key, structure in structures.items():
-            branches.append(self._generic_branch(x, structure))
+        for key, output in self.outputs.items():
+            branches.append(self._generic_branch(x, output))
         
-        model = Model(inputs=input_tensor,
+        self.model = Model(inputs=self.input_tensor,
                      outputs = branches,
-                     name="content_classification")        
-        
-        return model
+                     name="content_classification")
+    
+    def compile_model(self, opt):
+        """
+        """
+        self.optimizer = opt
 
-IM_WIDTH = 224
-IM_HEIGHT = 224
+        loss_dict = {}
+        loss_weights_dict = {}
+        metrics_dict = {}
 
-structures = {0: ('genre', 3, 'sigmoid'), 1: ('rating', 1, 'linear')}
-
-model = MultiOutputModel().build_model(IM_WIDTH, IM_HEIGHT, "InceptionV3", structures)
-keras.utils.plot_model(model, show_shapes=True)
+        for key, output in self.outputs.items():
+            loss_dict[output['type']] = output['loss']
+            loss_weights_dict[output['type']] = output['weight']
+            metrics_dict[output['type']] = output['metric']
+ 
+        self.model.compile(optimizer=self.optimizer, 
+              loss=loss_dict,
+              loss_weights=loss_weights_dict,
+              metrics=metrics_dict)
