7c43851edd17d6ba0b9d2c02c98b51ef337a5efb
Ghass <gam20@mail.aub.edu>
2021-05-10 21:10:58 +0200

    Added DataGenerator

--- cfgs/content_classification_v1.py
+++ cfgs/content_classification_v1.py
@@ -2,22 +2,23 @@ from .assets.default_configs import get_cfg_defaults
 
 _C = get_cfg_defaults()
 
-_C.MODEL.STRUCTURE = [{0: {'type': 'genre', 'outNeurons': 3, 'outActivation': 'sigmoid', 'loss': 'categorical_crossentropy', 'weight': 1, 'metric': 'accuracy'}, 
-                      1: {'type': 'rating', 'outNeurons': 1, 'outActivation': 'linear', 'loss': 'mse', 'weight': 1, 'metric': 'mae'}}]
+_C.MODEL.STRUCTURE = [{0: {'type': 'Genre', 'outNeurons': 3, 'outActivation': 'sigmoid', 'loss': 'categorical_crossentropy', 'weight': 1, 'metric': 'accuracy'}, 
+                      1: {'type': 'Rating', 'outNeurons': 1, 'outActivation': 'linear', 'loss': 'mse', 'weight': 1, 'metric': 'mae'},
+                      2: {'type': 'Year', 'outNeurons': 1, 'outActivation': 'linear', 'loss': 'mse', 'weight': 1, 'metric': 'mae'}}]
 
 _C.MODEL.BACKBONE = "InceptionV3"
 
 
 _C.IMAGE.RESOLUTION = (224, 224, 3)
 
-_C.HYPERPARAMS.TEST_SPLIT = 0.7
+_C.HYPERPARAMS.TEST_SPLIT = 0.8
+_C.HYPERPARAMS.VAL_SPLIT = 0.8
 _C.HYPERPARAMS.LR = 1e-4
 _C.HYPERPARAMS.EPOCHS = 100
 
-_C.CLASSES.BRANCH_1 = ['Comedy', 'Drama', 'Action', 'Documentary', 'Crime', 'Animation', 'Horror', 'Adventure']
-_C.CLASSES.BRANCH_2 = ['Rating']
-_C.CLASSES.BRANCH_2 = ['Year']
-
+_C.CLASSES = [{'Genre': {0: 'Comedy', 1: 'Drama', 2: 'Action', 3: 'Documentary', 4: 'Crime', 5: 'Animation', 6: 'Horror', 7:'Adventure'},
+                        'Rating': {0: 'value'},
+                        'Year': {0: 'value'}}]
 
 def get_cfg():
     return _C.clone()
---
+++ data_processing/data_generator.py
@@ -0,0 +1,79 @@
+# Inpired by the work of https://github.com/rodrigobressan
+# Modularity for any dataset/number of outputs coming soon !
+
+import cv2
+import numpy as np
+
+from tensorflow.keras.utils import to_categorical
+
+
+class DataGenerator(object):
+    """
+    """
+    def __init__(self, df, dataset_dict, train_test_split, train_val_split, img_size):
+        """
+        """
+        self.df = df
+        self.dataset_dict = dataset_dict
+
+        self.TRAIN_TEST_SPLIT = train_test_split
+        self.TRAIN_VAL_SPLIT = train_val_split
+        self.IMG_SIZE = img_size
+
+        self.max_rating = self.df['Rating'].max()
+        self.max_year = self.df['Year'].max()
+
+        self.df['Genre'] = self.df['Genre'].map(lambda genre: self.dataset_dict['Genre_alias'][genre])
+        
+    def split_dataset(self):
+        """
+        """
+        p = np.random.permutation(len(self.df))
+        train_up_to = int(len(self.df) * self.TRAIN_TEST_SPLIT)
+
+        train_idx = p[:train_up_to]
+        test_idx = p[train_up_to:]   
+
+        valid_idx = train_idx[int(train_up_to * self.TRAIN_VAL_SPLIT):]
+        train_idx = train_idx[:int(train_up_to * self.TRAIN_VAL_SPLIT)]
+        
+        return train_idx, valid_idx, test_idx
+    
+    def _process_image(self, img_path):
+        """
+        """
+        img = cv2.imread(image_path, 1)
+        img = cv2.resize(img, self.IMG_SIZE)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = np.array(img) / 255.0
+        
+        return img
+        
+    def generate_images(self, image_idx, is_training, batch_size):
+        """
+        """
+        images, genres, ratings, years = [], [], [], []
+
+        while True:
+            for idx in image_idx:
+                movie = self.df.iloc[idx]
+                
+                genre = movie['Genre']
+                rating = movie['Rating']
+                year = movie['Year']
+                image_path = movie['Poster_path']
+                
+                img = self._process_image(image_path)
+                
+                genres.append(to_categorical(genre, 8))
+                ratings.append(rating / self.rating)
+                years.append(year / self.max_year)
+                images.append(img)
+                
+                if len(images) >= batch_size:
+                    yield np.array(images), [np.array(genres), np.array(ratings), np.array(years)]
+
+                    images, genres, ratings, years = [], [], [], []
+                    
+            if not is_training:
+                break
